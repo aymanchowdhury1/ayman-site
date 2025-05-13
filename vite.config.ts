f4b14d49337f6f7d450a01665b9415b20f61ba36
aymanchowdhury1 <aymanc366@gmail.com>
2025-05-13 21:54:41 +0600

    Fixed GitHub Pages asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ayman-site/", // This should match your GitHub repo name
+  base: "/ayman-site/", // Must match your GitHub repo name
   plugins: [react()],
 });
